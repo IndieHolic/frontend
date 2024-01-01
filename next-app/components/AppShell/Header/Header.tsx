c924413daf3d8227ac788c0f1ede80ec16a7836c
BanYeah <126534852+BanYeah@users.noreply.github.com>
2024-01-01 22:23:25 +0900

    논의 후 새 디자인으로 헤더 완성

--- next-app/components/AppShell/Header/Header.tsx
+++ next-app/components/AppShell/Header/Header.tsx
@@ -1,87 +1,51 @@
-import {
-  IconBasket,
-  IconBell,
-  IconDeviceGamepad2,
-  IconNews,
-  IconSearch,
-  IconUpload,
-  IconUserSquareRounded,
-} from "@tabler/icons-react";
 import classes from "./Header.module.css";
-import {
-  Box,
-  Group,
-  Image,
-  UnstyledButton,
-  Text,
-  TextInput,
-} from "@mantine/core";
+import { Box, Group, Image, Text } from "@mantine/core";
+import { IconSearch } from "@tabler/icons-react";
 import Link from "next/link";
 
 export function Header() {
   return (
     <Box className={classes.Container}>
-      <Box className={classes.TopContainer}>
-        <Group className={classes.Wrapper} justify="space-between">
+      <Box className={classes.Wrapper}>
+        <Group gap={"50px"}>
           <Link href="/">
             <Image src="/images/header/agora-logo.svg" />
           </Link>
-          <Group gap={"10px"}>
-            <TextInput
-              classNames={{
-                root: classes.SearchRoot,
-                wrapper: classes.SearchRoot,
-                input: classes.SearchInput,
-                section: classes.SearchSection,
-              }}
-              placeholder="검색"
-              leftSection={
-                <IconSearch size={"20px"} stroke={2} color="#138BCF" />
-              }
-            />
-            <Text className={classes.WhiteMedium14}>·</Text>
-            <Link className={classes.ImageBellHover} href="/">
-              <Image src="/images/header/icon-bell.svg" />
+          <Group gap={"35px"}>
+            <Link className={classes.BlackSemiBold20} href="/">
+              게임
             </Link>
-            <Link href="/" className={classes.ImageUserHover}>
-              <Image src="/images/header/icon-user.svg" />
-              <Text className={classes.WhiteMedium14}>로그인</Text>
+            <Link className={classes.BlackSemiBold20} href="/">
+              에셋
+            </Link>
+            <Link className={classes.BlackSemiBold20} href="/">
+              커뮤니티
+            </Link>
+            <Link href="/" style={{ height: "20px" }}>
+              <IconSearch size={"20px"} stroke={2} />
             </Link>
           </Group>
         </Group>
-      </Box>
-      <Box className={classes.BottomContainer}>
-        <Group className={classes.Wrapper} justify="space-between">
-          {/* 좌측 컨텐츠 */}
-          <Group gap={"10px"}>
-            <Link className={classes.MenuTab} href="/">
-              <IconDeviceGamepad2 size={"24px"} stroke={1.5} />
-              <Text className={classes.BlackSemiBold16}>게임</Text>
+        <Group gap={"10px"}>
+          <Group gap={"20px"}>
+            <Link className={classes.BlackRegular14} href="/">
+              업로드
             </Link>
-            <Link className={classes.MenuTab} href="/">
-              <IconBasket size={"24px"} stroke={1.5} />
-              <Text className={classes.BlackSemiBold16}>에셋</Text>
-            </Link>
-            <Link className={classes.MenuTab} href="/">
-              <IconNews size={"24px"} stroke={1.5} />
-              <Text className={classes.BlackSemiBold16}>커뮤니티</Text>
+            <Link className={classes.BlackRegular14} href="/">
+              클라이언트 실행
             </Link>
           </Group>
-          {/* 우측 컨텐츠 */}
-          <Group gap={"13px"}>
-            <Link href="/">
-              <Group gap={"3px"}>
-                <IconUpload size={"22px"} stroke={1.5} />
-                <Text className={classes.BlackMedium14}>업로드</Text>
-              </Group>
-            </Link>
-            <UnstyledButton h={"22px"}>
-              <Group gap={"3px"}>
-                <Image src="/images/header/icon-client-play.svg" />
-                <Text className={classes.BlackMedium14}>클라이언트 실행</Text>
-              </Group>
-            </UnstyledButton>
-          </Group>
+          <Text className={classes.BlackRegular14}>·</Text>
+          <Link className={classes.ImageShoppingBag} href="/">
+            <Image src="/images/header/icon-shopping-bag.svg" />
+          </Link>
+          <Link className={classes.ImageBell} href="/">
+            <Image src="/images/header/icon-bell.svg" />
+          </Link>
+          <Link href="/" className={classes.ImageUser}>
+            <Image src="/images/header/icon-user.svg" />
+            <Text className={classes.BlackRegular14}>로그인</Text>
+          </Link>
         </Group>
       </Box>
     </Box>
