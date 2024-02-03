b64667c0ef34973dd83a9a5072a3ef2dccc2283a
BanYeah <126534852+BanYeah@users.noreply.github.com>
2024-02-03 22:01:55 +0900

    아고라 프리미엄 피그마 복붙 버전

---
+++ next-app/app/game/agora-premium/page.tsx
@@ -0,0 +1,10 @@
+import { ContentWrapper } from "@/components/Commons/ContentWrapper/ContentWrapper";
+import { Image } from "@mantine/core";
+
+export default function Home() {
+  return (
+    <ContentWrapper>
+      <Image src={"/images/temp.svg"} />
+    </ContentWrapper>
+  );
+}
